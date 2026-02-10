05b02bd5d352a8a5031d3e71026fb8ab79271d2f
chenthahour <hourchentha00@gmail.com>
2026-02-10 14:11:20 +0700

    updated latest1

--- src/app/pages/checkout/checkout-page/checkout-page.ts
+++ src/app/pages/checkout/checkout-page/checkout-page.ts
@@ -2,7 +2,6 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { CartService } from '../../../core/services/cart.service';
 import { PaymentMethod } from '../../../core/services/payment-method';
-import { BrowserModule } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-checkout-page',
@@ -11,14 +10,14 @@ import { BrowserModule } from '@angular/platform-browser';
   styleUrl: './checkout-page.scss',
 })
 export class CheckoutPage {
-  CartData: any;
+  CartData: any[] = [];
   TotalPrice: any;
   selectedMethod: any;
 
    payment_method:any;
 
   constructor(
-    private cartService: CartService,
+    public cartService: CartService,
     private paymentMethod: PaymentMethod
   ){
     
@@ -27,7 +26,6 @@ export class CheckoutPage {
   ngOnInit(){
     this.getPaymentMethod()
     this.getData();
-    this.totalPrice();
   }
 
   getData(){
@@ -35,32 +33,33 @@ export class CheckoutPage {
       (respone:any) =>{
         console.log('data cart', respone);
         this.CartData = respone[0]?.product; 
+
+        if(this.CartData){
+          this.totalPrice();
+        }
       }
     )
   }
 
 
-  getPaymentMethod(){
-    this.payment_method = this.paymentMethod.getMethods();
-    console.log('payment method', this.payment_method)
+  totalPrice(): number {
+    return this.CartData?.reduce((total, item) => {
+      return total + (item.price * item.qty);
+    }, 0);
   }
 
 
-  //show total price 
-  totalPrice(){
-    this.TotalPrice = this.cartService.total();
-  }
-
-
-  //increase cart
+    //increase cart
   increase(p: any) {
     p.qty++;
+    this.UpdatedAllData(p);
   }
 
 
   //decrease cart
   decrease(p: any) {
     if (p.qty > 0) p.qty--;
+     this.UpdatedAllData(p);
   }
 
   //on input in or de cart
@@ -73,6 +72,40 @@ export class CheckoutPage {
     } else {
       p.qty = value;
     }
+  }
+
+
+    //update data added
+  UpdatedAllData(data: any) {
+    const index = this.CartData.findIndex(item => item.id === data.id);
+
+    if (data.qty > 0) {
+      if (index === -1) {
+        this.CartData.push({ ...data });
+      } else {
+        this.CartData[index].qty = data.qty; 
+      }
+    } else {
+      if (index !== -1) {
+        this.CartData.splice(index, 1); 
+      }
+    }
+
+    console.log('CartData:', this.CartData);
+  }
+
+
+  //update qty product
+  updateCart(data:any){
+    this.cartService.updateQty(data.id, data.qty);
+  }
+
+
+
+
+  getPaymentMethod(){
+    this.payment_method = this.paymentMethod.getMethods();
+    console.log('payment method', this.payment_method)
   }
 
 
