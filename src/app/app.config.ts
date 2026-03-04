95f81b7c110e4a9ff734f55c2eb345c482658883
chenthahour <hourchentha00@gmail.com>
2026-03-04 12:40:26 +0700

    updated 14

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,10 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { APP_INITIALIZER, ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { JwtInterceptor } from './core/helper/jwt.interceptor';
+import { Auth } from './core/services/auth';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -11,6 +12,12 @@ export const appConfig: ApplicationConfig = {
     provideRouter(routes),
     provideHttpClient(
       withInterceptors([JwtInterceptor])
-    )
+    )   
+    ,{
+      provide: APP_INITIALIZER,
+      useFactory: (auth: Auth) => () => auth.initToken(),
+      deps: [Auth],
+      multi: true
+    }
   ]
 };
